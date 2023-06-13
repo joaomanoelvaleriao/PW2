218675238126056a60370821941f60f7da013654
joaomanoelvaleriano <126586711+joaomanoelvaleriao@users.noreply.github.com>
2023-06-13 12:22:18 -0300

    Add files via upload

---
+++ Aula 13_06_2023/projetoweb/index.php
@@ -0,0 +1,30 @@
+<?php
+ 
+include("conexao.php");
+
+$nome = $_GET['nome'];
+/*$endereco = $_GET['endereco'];
+$bairro = $_GET['bairro'];
+$cep = $_GET['cep'];
+$cidade = $_GET['cidade'];
+$estado = $_GET['estado'];
+
+$sql = "INSERT INTO clientes (nome, endereco, bairro, cep, cidade, estado) VALUES (":nome, :endereco, :bairro, :cep, :cidade, :estado");*/
+$sql = "INSERT INTO clientes(nome) VALUES(:nome);";
+
+$stmt = $PDO ->prepare($sql);
+
+$stmt->bindParam(':nome', $nome);
+/*$stmt->bindParam(':endereco', $endereco);
+$stmt->bindParam(':bairro', $bairro);
+$stmt->bindParam(':cep', $cep);
+$stmt->bindParam(':cidade', $cidade);
+$stmt->bindParam(':estado', $estado);*/
+
+if($stmt->execute()){
+    echo "Dados Gravados com Sucesso!";
+}else{
+    echo "Erro ao Gravar Dados";
+}
+
+?>
